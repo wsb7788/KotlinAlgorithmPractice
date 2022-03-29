89c237cc680694057c7913757331dcb30ea1a7cc
wsb7788 <wsb0514@naver.com>
2022-03-29 19:02:49 +0900

    baekjoon 1965

---
+++ 백준/1965.kt
@@ -0,0 +1,31 @@
+/*문제
+정육면체 모양의 상자가 일렬로 늘어서 있다. 상자마다 크기가 주어져 있는데, 앞에 있는 상자의 크기가 뒤에 있는 상자의 크기보다 작으면, 앞에 있는 상자를 뒤에 있는 상자 안에 넣을 수가 있다. 예를 들어 앞에서부터 순서대로 크기가 (1, 5, 2, 3, 7)인 5개의 상자가 있다면, 크기 1인 상자를 크기 5인 상자에 넣고, 다시 이 상자를 크기 7인 상자 안에 넣을 수 있다. 하지만 이렇게 상자를 넣을 수 있는 방법은 여러 가지가 있을 수 있다. 앞의 예에서 차례대로 크기가 1, 2, 3, 7인 상자를 선택하면 총 4개의 상자가 한 개의 상자에 들어가게 된다.
+
+상자의 크기가 주어질 때, 한 번에 넣을 수 있는 최대의 상자 개수를 출력하는 프로그램을 작성하시오.
+
+입력
+파일의 첫 번째 줄은 상자의 개수 n (1 ≤ n ≤ 1000)을 나타낸다. 두 번째 줄에는 각 상자의 크기가 순서대로 주어진다. 상자의 크기는 1,000을 넘지 않는 자연수이다.
+
+출력
+첫째 줄에 한 줄에 넣을 수 있는 최대의 상자 개수를 출력한다.*/
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.Integer.max
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    val n = readLine().toInt()
+    val box = readLine().split(" ").map{it.toInt()}
+    val dp = IntArray(n)
+    var count = 0
+    for (i in 0 until n) {
+        dp[i] = 1
+        for( j in 0 until i ){
+            if(box[j]<box[i]){
+                dp[i] = max(dp[j]+1, dp[i])
+            }
+        }
+        count = max(count, dp[i])
+    }
+
+    println(count)
+}
