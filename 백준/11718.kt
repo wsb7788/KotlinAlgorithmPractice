73a315829a7c6da8d1bf31e371416976c6bf2be3
wsb7788 <wsb0514@naver.com>
2022-05-13 23:44:18 +0900

    baekjoon 11718

---
+++ 백준/11718.kt
@@ -0,0 +1,9 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))){
+    while(true){
+        println(readLine())
+    }
+
+}
